25c59c990bd177dfa769c0352d743f90c01642a9
abelixrev <abel.tarrago@ixrev.com>
2025-01-08 15:05:20 +0100

    deleted build folder and add build folder to gitignore

--- build/bootmode1/fpgenprogProject/proj_fp/proj_fp.tcl
+++
@@ -1,7 +0,0 @@
-open_project -project {C:\Users\admin\abel\07-hart_services\hart-software-services\build/bootmode1/fpgenprogProject\proj_fp\proj_fp.pro}
-enable_device -name {target} -enable 1
-set_programming_file -name {target} -file {C:\Users\admin\abel\07-hart_services\hart-software-services\build/bootmode1/fpgenprogProject\proj_fp\target.ppd}
-set_programming_action -action {PROGRAM} -name {target}
-run_selected_actions
-save_project
-close_project
--- build/services/opensbi/opensbi_ecall_exts.c
+++
@@ -1,9 +0,0 @@
-#include <sbi/sbi_ecall.h>
-
-extern struct sbi_ecall_extension ecall_base;
-
-struct sbi_ecall_extension *sbi_ecall_exts[] = {
-	&ecall_base,
-};
-
-unsigned long sbi_ecall_exts_size = sizeof(sbi_ecall_exts) / sizeof(struct sbi_ecall_extension *);
